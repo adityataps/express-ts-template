df681a6fe491d0c2edde8a05e7950009e8624933
Aditya Tapshalkar <aditya.taps@gmail.com>
2024-09-26 00:37:36 -0400

    chore: set up express api boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello world');
+/**
+ * @file @/index.ts
+ * @description Express entrypoint for the application
+ * @
+ */
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json('Hello world');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
